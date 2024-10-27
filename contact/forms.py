9afca86ac1b2b99457ff349551bf00ec345959c3
cuesta0900 <pedropcs608@gmail.com>
2024-10-27 19:21:32 -0300

    form creation

---
+++ contact/forms.py
@@ -0,0 +1,46 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from contact import models
+
+
+class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(#sobrescreve o first_name da Model
+        widget=forms.TextInput(
+            attrs={ #atributos do HTML
+                'class': 'classe-html',
+                'placeholder': 'Escreve aqui...'
+            }
+        ),
+        label = 'Primeiro Nome',
+        help_text='Texto de ajuda para o usuário'
+    )
+    
+    class Meta:
+        model = models.Contact
+        fields = (#tuple
+            'first_name', 'last_name', 'phone', 'email', 'description', 'category'
+        )
+        
+    def clean(self): #tratativa dos erros do form
+        cleaned_data = self.cleaned_data #leitura dos dados enviados
+        #erros que serão exibidos, ainda sem validação
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        
+        if first_name == last_name:
+            msg = ValidationError('Primeio nome não pode ser igual ao último', code='invalid')
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
+            
+        return super().clean()
+    
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        if first_name == 'ABC':
+            self.add_error(
+                'first_name',
+                ValidationError(
+                    'Não digite ABC',
+                    code='invalid'
+                ))
+        return first_name
--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,21 +1,20 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator
-from django.db.models import Q
-from contact.models import Contact
-
-
+from django.shortcuts import render
+from contact.forms import ContactForm
 def create(request):
     if request.method == 'POST':
-        print(request.POST.get('first_name'))   
-        print(request.POST.get('last_name'))
-       
-    context = {
+        context = {
+            'form': ContactForm(request.POST)
+        }
         
-    }
+        return render(
+            request, 
+            'contact/create.html',
+            context
+        )
     
-    print()
-    print(request.method) #Podemos usar isso para indicicar o tipo de retorno
-    print()
+    context = {
+        'form': ContactForm
+    }
     
     return render(
         request, 
