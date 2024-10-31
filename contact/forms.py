5c90aedaf6a82166283ce414d33ed357743cfd38
cuesta0900 <pedropcs608@gmail.com>
2024-10-30 23:28:56 -0300

    User Creation

--- contact/forms.py
+++ contact/forms.py
@@ -1,7 +1,8 @@
 from django import forms
 from django.core.exceptions import ValidationError
 from contact import models
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ContactForm(forms.ModelForm):
     picture = forms.ImageField(
@@ -54,3 +55,28 @@ class ContactForm(forms.ModelForm):
                     code='invalid'
                 ))
         return first_name
+    
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True
+    )
+    last_name = forms.CharField(
+        required=True
+    )
+    email = forms.EmailField(
+        required=True
+    )
+    
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2',)
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Email está sendo usado por outro usuário', 
+                                code='invalid')
+                )
+        return email
