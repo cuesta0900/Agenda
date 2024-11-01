be861366a08cab1cc7c3731f09039979c60e0788
cuesta0900 <pedropcs608@gmail.com>
2024-10-31 22:15:41 -0300

    login page

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from contact.forms import RegisterForm
-from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth, messages
 
 def register(request):
     form = RegisterForm()
@@ -21,3 +22,29 @@ def register(request):
             'form': form
         }
     )
+    
+def login_view(request):
+    form = AuthenticationForm(request)
+    
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            messages.success(request, 'Logado com sucesso!')
+            return redirect('contact:index')
+        messages.error(request, 'contact/login.html')
+            
+            
+    return render(
+        request, 
+        'contact/login.html',
+        context={
+            'form': form
+        }
+    )
+    
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
