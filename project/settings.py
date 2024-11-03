f9f93f25ea1e5d42d1d4f563da4cf2bad45f8a10
cuesta0900 <pedropcs608@gmail.com>
2024-11-03 20:51:26 -0300

    loading static

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fvxm5d#5o_nm_+o2bi!_ubr0y+q=^1-5mql&8s)a5-4m1733e('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
