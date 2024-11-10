255565790b4fca2c00c4265fd6b8ac5191f435e0
cuesta0900 <pedropcs608@gmail.com>
2024-11-09 23:40:56 -0300

    Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Agenda.wsgi:application --bind 0.0.0.0:$PORT
--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+PORT = os.getenv('PORT', 8000)
 # Application definition
 
 INSTALLED_APPS = [
